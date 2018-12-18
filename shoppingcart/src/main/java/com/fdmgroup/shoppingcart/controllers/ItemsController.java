15351b249ca5f2471f9578a62a720923c998e55c
salomeaka <salome.aka@gmail.com>
2018-12-18 13:03:54 +0000

    Changed faulty getOne to findById in controller

--- shoppingcart/src/main/java/com/fdmgroup/shoppingcart/controllers/ItemsController.java
+++ shoppingcart/src/main/java/com/fdmgroup/shoppingcart/controllers/ItemsController.java
@@ -5,17 +5,15 @@ import com.fdmgroup.shoppingcart.models.Item;
 import com.fdmgroup.shoppingcart.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/")
@@ -36,8 +34,8 @@ public class ItemsController {
     }
 
     @RequestMapping(value = "items/{id}", method = RequestMethod.GET)
-    public Item get(@PathVariable("id") long id){
-        return itemRepository.getOne(id);
+    public Optional<Item> get(@PathVariable("id") long id){
+        return itemRepository.findById(id);
     }
 
 
