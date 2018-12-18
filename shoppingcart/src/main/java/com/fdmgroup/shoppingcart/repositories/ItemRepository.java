4f32feedf3a89ae8d86ae83d54f05b6ccdf478bb
salomeaka <salome.aka@gmail.com>
2018-12-18 22:08:31 +0000

    Unit testing, solved html errors, started on cart

--- shoppingcart/src/main/java/com/fdmgroup/shoppingcart/repositories/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fdmgroup.shoppingcart.repositories;
-
-import com.fdmgroup.shoppingcart.models.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
--- shoppingcart/src/main/resources/db/migration/V3__create_item.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO ITEM VALUES (1,'Apple', 'Sweet and juicy, perfect for pies', 2.20, 25);
-INSERT INTO ITEM VALUES (2,'Bacon', 'Smoked back rahsers', 3.25, 19);
-INSERT INTO ITEM VALUES (3,'Milk', 'Organic semi-skimmed milk', 1.85, 33);
-INSERT INTO ITEM VALUES (4,'Bread', 'Rye sourdough', 1.05, 2);
